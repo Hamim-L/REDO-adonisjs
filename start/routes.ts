5ecf4aedb0e5a8abf29bfdd8e7aea403feb0d8ca
unknown <hamimmr12@gmail.com>
2025-10-16 23:30:19 +0700

    nambahin dikit

--- start/routes.ts
+++ start/routes.ts
@@ -12,3 +12,5 @@ import router from '@adonisjs/core/services/router'
 router.on('/').render('pages/home')
 router.on('/about').render('pages/about')
 router.on('/services').render('pages/services')
+router.on('/home').render('pages/home')
+router.on('/contact').render('pages/contact')
